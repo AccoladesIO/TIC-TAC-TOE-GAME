9b8e79e8947b622c99fc96f958a01afe9b7de4c0
unknown <ononobip@gmail.com>
2022-09-26 23:10:11 +0100

    New commit

--- src/components/score/score.js
+++ src/components/score/score.js
@@ -2,8 +2,9 @@ import React from 'react'
 import './score.css'
 
 const Score = ({value}) => {
+    const scoreTab = value === "X"? "x" : "o"
   return (
-    <div className='scoreTab'>Winner: {value}</div>
+    <div className="scoreTab" >Winner: <p className={scoreTab}>{value}</p></div>
   )
 }
 
